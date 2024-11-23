626b627d617c821aa6e98ef1f1cebea21201d6bb
brody <ghbcw424@gmail.com>
2024-11-24 08:24:59 +0900

    Initial Commit

---
+++ TeamProject1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamProject1
+//
+//  Created by 변창원 on 11/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TeamProject1Tests/TeamProject1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TeamProject1Tests.swift
+//  TeamProject1Tests
+//
+//  Created by 변창원 on 11/24/24.
+//
+
+import Testing
+@testable import TeamProject1
+
+struct TeamProject1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
