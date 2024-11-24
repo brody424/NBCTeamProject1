af5aa108fc1da33910dfe7657b773f73c1f4b508
brody <ghbcw424@gmail.com>
2024-11-24 09:08:44 +0900

    First View Controller 만들어서 작업 시작합니다!

---
+++ TeamProject1/FirstViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.swift
+//  TeamProject1
+//
+//  Created by 변창원 on 11/24/24.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // 계정A에서 FirstViewController 화면을 만들어서 개발을 시작했어요
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
