f17c0c466decbfd9a130a88728b5356fdde2f5a4
kangminseoung <alstjd2356@gmail.com>
2024-11-25 12:15:30 +0900

    kms

---
+++ TeamProject1/kmsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  kmsViewController.swift
+//  TeamProject1
+//
+//  Created by 강민성 on 11/25/24.
+//
+
+import UIKit
+
+class kmsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
